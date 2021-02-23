5885224253a9b490dbfcb08243e8020b6c82c999
whereislima <reach.lima@gmail.com>
2021-02-22 22:21:27 -0800

    first commit

---
+++ calculator/calculator.js
@@ -0,0 +1,73 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById("calc-form");
+  if (form) {
+    setupIntialValues();
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      update();
+    });
+  }
+});
+
+function getCurrentUIValues() {
+  return {
+    amount: +(document.getElementById("loan-amount").value),
+    years: +(document.getElementById("loan-years").value),
+    rate: +(document.getElementById("loan-rate").value),
+  }
+
+  // console.log(document.getElementById("loan-amount").value, document.getElementById("loan-years").value, document.getElementById("loan-rate").value);
+  // const values = {
+  //   amount: +(document.getElementById("loan-amount").value),
+  //   years: +(document.getElementById("loan-years").value),
+  //   rate: +(document.getElementById("loan-rate").value),
+  // }
+  // console.log(values.amount, values.years, values.rate);
+  // calculateMonthlyPayment(values)
+}
+
+// Get the inputs from the DOM.
+// Put some default values in the inputs
+// Call a function to calculate the current monthly payment
+function setupIntialValues() {
+  // const amount = document.getElementById("loan-amount").value = 100000;
+  // const years = document.getElementById("loan-years").value = 15;
+  // const rate = document.getElementById("loan-rate").value = 2.4;
+  const values = {
+    amount: document.getElementById("loan-amount").value = 100000,
+    years: document.getElementById("loan-years").value = 15,
+    rate: document.getElementById("loan-rate").value = 4,
+  }
+  calculateMonthlyPayment(values);
+}
+
+// Get the current values from the UI
+// Update the monthly payment
+function update() {
+  let inputs = getCurrentUIValues();
+  let calculated = calculateMonthlyPayment(inputs);
+  update(calculated);
+}
+
+// Given an object of values (a value has amount, years and rate ),
+// calculate the monthly payment.  The output should be a string
+// that always has 2 decimal places.
+function calculateMonthlyPayment(values) {
+  const principle = values.amount;
+  const interest = (values.rate/100)/12; 
+  const numOfPay = (values.years) * 12;
+  const payment = (principle * interest) / (1 - Math.pow((1 + interest), -numOfPay));
+  const finalPayment = payment.toFixed(2);
+  // console.log(finalPayment);
+  updateMonthly(finalPayment);
+}
+
+// Given a string representing the monthly payment value,
+// update the UI to show the value.
+function updateMonthly(monthly) {
+  const monthlyPayment = document.getElementById("monthly-payment");
+  monthlyPayment.innerText = `$ ${monthly}`;
+}
+
+
+// need to fix update and getcurrentuivalues;
---
+++ tip-pool/servers.test.js
@@ -0,0 +1,32 @@
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
+
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+  });
+
+  afterEach(function() {
+    // teardown logic
+    serverNameInput.value = '';
+  });
+});
+
+
+describe("", function() {
+  beforeEach(function () {
+
+  });
+  it('should', function () {
+    updateServerTable();
+    expect().toEqual();
+  });
+  afterEach(function() {
+
+  });
+});
